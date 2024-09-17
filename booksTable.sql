5f34cc1d6980488189e9402d36006909d4c59437
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-09-17 14:04:39 +0200

    Add ex create books table

---
+++ booksTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Books (
+  book_id INTEGER,
+  title TEXT,
+  author TEXT,
+  genre TEXT,
+  published_year INTEGER,
+  isbn TEXT,
+  price INTEGER,
+  rating INTEGER,
+  stock_count INTEGER
+)
+/*
+Immagina di progettare un database per gestire una libreria. 
+Crea una tabella denominata "Books" con attributi per 
+book_id, title, author, genre, published_year, isbn, price, rating e stock_count.
+*/
