022b38e025b2467fa4108fe6091f0addef64df64
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-09-17 14:45:38 +0200

    Change file in txt

--- addColumns.sql
+++
@@ -1,7 +0,0 @@
-alter table Books add column publisher TEXT;
-alter TABLE Books add column number_pages INTEGER
-
-/*
-You want to add more details about each book. 
-Modify the "Books" table to include columns for the publisher and the number of pages.
-*/
